4c82a22d53698c6ac062905f3b2d3d8e4931dd79
Garrick Henne <garrickhenne@outlook.com>
2023-10-05 16:27:57 -0700

    Dev: added example PlayerBoard and print coordinates on start

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const PlayerBoard = require('./model/PlayerBoard');
 
 const app = express();
 const PORT = 10001;
@@ -14,4 +15,8 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}. Link to site: https://localhost:${PORT}`);
+
+  // For dev purposes just create a new board now and print it.
+  const newBoard = new PlayerBoard();
+  console.log(newBoard.printCoordinates());
 });
