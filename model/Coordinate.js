4f52d9291827c7bdeba6d76580b05853dd8e420d
Garrick Henne <garrickhenne@outlook.com>
2023-10-05 16:51:10 -0700

    Coordinate to naming convention

--- model/Coordinate.js
+++ model/Coordinate.js
@@ -1,9 +1,19 @@
+const Point = require('./Point');
+
 class Coordinate {
   // x denotes row [1-10] by number.
   // y denotes column [A-J] by number.
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this._x = new Point(x);
+    this._y = new Point(y);
+  }
+
+  get x() {
+    return this._x.toXCoordinate();
+  }
+
+  get y() {
+    return this._y.toYCoordinate();
   }
 }
 
---
+++ model/Point.js
@@ -0,0 +1,20 @@
+class Point {
+  constructor(point) {
+    if (point > 9 || point < 0 || typeof point !== 'number') {
+      throw Error('Point is not valid.');
+    }
+    this._point = point;
+  }
+
+  // [1, 9]
+  toXCoordinate() {
+    return this._point + 1;
+  }
+
+  // [A, J]
+  toYCoordinate() {
+    return (this._point + 10).toString(36).toUpperCase();
+  }
+}
+
+module.exports = Point;
