2a607ed953a37e8114b6a37017f92143c312536e
Garrick Henne <garrickhenne@outlook.com>
2023-10-06 13:19:24 -0700

    Install ejs, add public folder to load basic css files

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
-const PlayerBoard = require('./model/PlayerBoard');
+// const PlayerBoard = require('./model/PlayerBoard');
+// const Battleship = require('./model/Battleship');
+// const Coordinate = require('./model/Coordinate');
+const { initGame } = require('./util/GameFactory');
 
 const app = express();
 const PORT = 10001;
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hit point.');
-  // do nothing
+  const newGame = initGame();
+  // renderBoard(newGame.player1.board);
+  res.render('home', { board: newGame.player1.board });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}. Link to site: https://localhost:${PORT}`);
 
   // For dev purposes just create a new board now and print it.
-  const newBoard = new PlayerBoard();
-  console.log(newBoard.printCoordinates());
+  // const game = createGame()
 });
